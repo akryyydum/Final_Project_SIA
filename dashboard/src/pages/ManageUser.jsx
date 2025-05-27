19fe2c5dafac6e3fe9dd28f335ca2fd46965d0e1
Felix C. Leid Jr. <hed-fleidjr@smu.edu.ph>
2025-05-27 08:16:38 +0800

    we

--- dashboard/src/pages/ManageUser.jsx
+++ dashboard/src/pages/ManageUser.jsx
@@ -7,17 +7,6 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useAuth } from "../context/AuthContext";
 
-const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  borderRadius: 2,
-  boxShadow: 24,
-  p: 4,
-};
 
 const ManageUser = () => {
   const [users, setUsers] = useState([]);
